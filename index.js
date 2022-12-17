97fdebff4f9cdf32d7b3c2830e4fb8404c45b2b4
brian m lam <brian.michael.lam@gmail.com>
2022-12-17 00:10:24 -0800

    removed data/index.js dummy data file

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname,'/client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api running')
+  })
+}
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
