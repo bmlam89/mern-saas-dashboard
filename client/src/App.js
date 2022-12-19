1124b058de7ebc1028df69585aab12594cea34a8
brian m lam <brian.michael.lam@gmail.com>
2022-12-19 01:45:17 -0800

    Added global theme via material ui to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { CssBaseline, ThemeProvider } from '@mui/core-downloads-tracker';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <h1>
-        Hello
-      </h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <h1>
+          Hello
+        </h1>
+      </ThemeProvider>
+      
     </div>
   );
 }
